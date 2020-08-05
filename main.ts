7204b0a55465a702f209e9c764f6fb1c1e09aa4a
jdeduq <51926216+jdeduq@users.noreply.github.com>
2020-08-05 10:39:26 -0400

    cleanup

--- main.ts
+++ main.ts
@@ -1,14 +1,4 @@
 let prediction = ""
-input.onButtonPressed(Button.A, function () {
-    serial.writeLine("Button A pressed")
-    basic.showString("A")
-    basic.clearScreen()
-})
-input.onButtonPressed(Button.B, function () {
-    serial.writeLine("Button B pressed")
-    basic.showString("B")
-    basic.clearScreen()
-})
 basic.forever(function () {
     prediction = serial.readLine()
     if (prediction == "Go") {
