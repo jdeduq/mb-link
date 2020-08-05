8b8143e49a8ff3e7dcba691b3f3b3b7236f0a6ba
jdeduq <51926216+jdeduq@users.noreply.github.com>
2020-08-04 21:57:31 -0400

    Read/Write added

--- main.ts
+++ main.ts
@@ -1,8 +1,16 @@
 let prediction = ""
 input.onButtonPressed(Button.A, function () {
-    serial.writeLine("mb-tx")
+    serial.writeLine("Button A pressed")
+    basic.showString("A")
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    serial.writeLine("Button B pressed")
+    basic.showString("B")
+    basic.clearScreen()
 })
 basic.forever(function () {
     prediction = serial.readLine()
     basic.showString(prediction)
+    basic.clearScreen()
 })
